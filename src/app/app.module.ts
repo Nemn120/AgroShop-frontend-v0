dfee6a9c357a14896e68c1516e36b2ec415067b8
Fernando Chavez <alvaro1221x@gmail.com>
2020-10-26 20:53:00 -0500

    add components and rest service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductListComponent } from './pages/admin/product/product-list/product-list.component';
+import { ProductFormComponent } from './pages/admin/product/product-form/product-form.component';
+import { ProductNavbarComponent } from './pages/admin/product/product-navbar/product-navbar.component';
+import { RegistryComponent } from './pages/authorization/registry/registry.component';
+import { LoginComponent } from './pages/authorization/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductFormComponent,
+    ProductNavbarComponent,
+    RegistryComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,23 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  HOST:"https://pallevar-backend-2020.herokuapp.com", // 
+  //HOST:"http://localhost:8080", 
+  //HOST: "https://hidden-wildwood-21108.herokuapp.com",
+  //HOST: "https://pallevar-backend-2020.herokuapp.com",
+
+  TOKEN_NAME : 'access_token',
+  TOKEN_AUTH_USERNAME: 'pallevarR',
+  TOKEN_AUTH_PASSWORD: 'pallevarx',
+  REINTENTOS: 3,
+  MICRO_CRUD: 'micro-crud',
+  MICRO_CR : 'micro-cr',
+  MICRO_AUTH : 'uaa',
+
+  HOST_MAPBOX:'https://api.mapbox.com',
+  TOKEN_MAPBOX:'pk.eyJ1Ijoiam9zZWNvbmRvcmk1IiwiYSI6ImNrZTR0cW5nZzB3bjkyeXBkZXlmank2a3kifQ.z8hPiaEmZpSaEV67dtKlkw'
 };
 
 /*
