062f23988d9066e2951524c519e0c18a79736f3b
Osti Katriel <katriel181920@gmail.com>
2020-10-26 23:40:30 -0500

    component central-view created

---
+++ src/app/pages/authorization/login/central-view/central-view.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CentralViewComponent } from './central-view.component';
+
+describe('CentralViewComponent', () => {
+  let component: CentralViewComponent;
+  let fixture: ComponentFixture<CentralViewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CentralViewComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CentralViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
